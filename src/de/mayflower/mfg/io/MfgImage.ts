fbd7bba7e23ccf8c7353afcaf1695e64fde69c5e
jakry <jakry@web.de>
2015-07-31 11:57:57 +0200

    change image for my lovely christoph

--- src/de/mayflower/mfg/io/MfgImage.ts
+++ src/de/mayflower/mfg/io/MfgImage.ts
@@ -1,23 +1,26 @@
 class MfgImage
 {
-    private static image:HTMLImageElement = null;
+    public static PLAYER1_LEFT = "standLeft.png";
+    public static PLAYER1_RIGHT = "standRight.png";
+    public static PLAYER1_WALKLEFT = "walkLeft.png";
+    public static PLAYER1_WALKRIGHT = "walkRight.png";
+
+    private static player1:HTMLImageElement = null;
 
     public static getImage():HTMLImageElement
     {
-        return MfgImage.image;
+        return MfgImage.player1;
     }
 
-    public static loadImage(callback:() => void):void
+    public static loadImage(callback:any):void
     {
         var src:string = "res/image/player1.png";
 
-        //try drawing an image
-        var img = new Image();
-        img.src = src;
-        img.onload = function(){
+        //try drawing an imageheight
+        MfgImage.player1 = new HTMLImageElement();
+        MfgImage.player1.src = src;
+        MfgImage.player1.onload = function(){
             callback();
         };
-
-        MfgImage.image = img;
     }
 }
