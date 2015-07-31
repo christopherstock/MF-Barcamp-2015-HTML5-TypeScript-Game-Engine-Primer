930c14ae073041c1639ce823cbf5939eaf66ab43
Christopher Stock <email@christopherstock.de>
2015-07-31 10:36:05 +0200

    Added documentation for the LibDrawing-class.

--- src_lib/de/mayflower/lib/ui/LibDrawing.ts
+++ src_lib/de/mayflower/lib/ui/LibDrawing.ts
@@ -1,17 +1,32 @@
 
+    /*****************************************************************************
+    *   Specifies all independent drawing operations.
+    *
+    *   @author     Christopher Stock
+    *   @version    0.0.1
+    *****************************************************************************/
     class LibDrawing
     {
-        /**
-         * Fills a rect on canvas
-         *
-         * @param ctx
-         * @param color
-         * @param x
-         * @param y
-         * @param width
-         * @param height
-         */
-        public static fillRect( ctx:CanvasRenderingContext2D, color:string, x:number, y:number, width:number, height:number ):void
+        /*****************************************************************************
+        *   Fills a rect on canvas
+        *
+        *   @param  ctx     The canvas drawing context.
+        *   @param  color   The color to fill the rect with.
+        *   @param  x       The rect's X.
+        *   @param  y       The rect's Y.
+        *   @param  width   The rect's width.
+        *   @param  height  The rect's height.
+        *****************************************************************************/
+        public static fillRect
+        (
+            ctx:CanvasRenderingContext2D,
+            color:string,
+            x:number,
+            y:number,
+            width:number,
+            height:number
+        )
+        :void
         {
             ctx.fillStyle = color;
             ctx.fillRect( x, y, width, height );
