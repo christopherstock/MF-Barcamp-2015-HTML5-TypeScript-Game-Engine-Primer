2f2f949a5993bd7a1979847a894d6249ce916c0e
Christopher Stock <email@christopherstock.de>
2015-07-31 10:21:03 +0200

    Added documentation for the key system.

--- src_lib/de/mayflower/lib/key_press/LibKeySystem.ts
+++ src_lib/de/mayflower/lib/key_press/LibKeySystem.ts
@@ -1,25 +1,41 @@
 
+    /*****************************************************************************
+    *   Represents the key system that provides independent multiple key presses.
+    *
+    *   @author     Christopher Stock
+    *   @version    0.0.1
+    *****************************************************************************/
     class LibKeySystem
     {
-        private static              keys:Array<boolean>                 = null;
+        /** Stores all pressed keys. */
+        private         static              keys:Array<boolean>                 = null;
 
-
+        /*****************************************************************************
+        *   Inits the key system.
+        *****************************************************************************/
         public static init()
         {
             LibKeySystem.keys = new Array<boolean>();
 
-            window.onkeydown = function (e)
+            window.onkeydown = function( e )
             {
-                LibKeySystem.keys[e.keyCode] = true;
+                LibKeySystem.keys[ e.keyCode ] = true;
             };
 
-            window.onkeyup = function (e)
+            window.onkeyup = function( e )
             {
-                LibKeySystem.keys[e.keyCode] = false;
+                LibKeySystem.keys[ e.keyCode ] = false;
             };
         }
 
-        public static isKeyPressed(keyCode:number)
+        /*****************************************************************************
+        *   Checks if the specified key is pressed.
+        *
+        *   @param  keyCode The keycode of the key to check the pressed state for.
+        *   @return         <code>true</code> if this key is currently pressed.
+        *                   Otherwise <code>false</code>.
+        *****************************************************************************/
+        public static isKeyPressed( keyCode:number )
         {
             return LibKeySystem.keys[ keyCode ];
         }
